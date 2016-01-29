c82ee1147466f210bce4b8589d4a9b1a55951e3d
daniellelara <daniellelaragourgey@gmail.com>
2016-01-29 15:36:43 +0000

    first player finished

--- js/app.js
+++ js/app.js
@@ -2,141 +2,55 @@ console.log("its");
 $(function() {
 
 
-     $('#pieceOneCopy').draggable();
 
-    $('#pieceOne').droppable({
+  $('.draw-piece').draggable();
+
+  $('.puzzle-piece').droppable({
         drop: function(ev, ui) {
             var dropped = ui.draggable;
             var droppedOn = $(this);
             $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceOneCopy').draggable({
-  disabled: true
+            console.log($(dropped).attr('id').match($(droppedOn).attr('id')));
+            if (!!$(dropped).attr('id').match($(droppedOn).attr('id'))){
+                            $(dropped).draggable({
+                              disabled: true
+                              });
+           }
+           checkForWin(); 
+         } 
+
+    });
+
+  var $countdown = $('#screenTwo');
+  var count = 0
+  var countUp = setInterval(function(){
+      count += 1
+      var padOne = pad(count);
+      $countdown.text(padOne);
+    
+
+  },1000);
+  
+  
+
+
+
+
+  
+
 });
 
 
-        }
-    });
+function pad(count) {     return (count<10) ? ("0" + count.toString()) : (count.toString());   } 
 
-     $('#pieceTwoCopy').draggable();
+function checkForWin() {
+  var howMany = $('.ui-draggable-disabled').length;
+  if (howMany === 9){
+    console.log("game over. well done");
+    clearTimeout(countUp);
+  }
 
-    $('#pieceTwo').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceTwoCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-     $('#pieceThreeCopy').draggable();
+}
 
-    $('#pieceThree').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceThreeCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-     $('#pieceFourCopy').draggable();
-
-    $('#pieceFour').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceFourCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-
-     $('#pieceFiveCopy').draggable();
-
-    $('#pieceFive').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceFiveCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-     $('#pieceSixCopy').draggable();
-
-    $('#pieceSix').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceSixCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-     $('#pieceSevenCopy').draggable();
-
-    $('#pieceSeven').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceSevenCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-     $('#pieceEightCopy').draggable();
-
-    $('#pieceEight').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceEightCopy').draggable({
-              disabled: true
-            });
-        }
-    });
-     $('#pieceNineCopy').draggable();
-
-    $('#pieceNine').droppable({
-        drop: function(ev, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-            $('#pieceNineCopy').draggable({
-              disabled: true
-            });
-
-        }
-    });
-      
-
-  });
-
-
-
-
-/*$( ".draw-piece" ).draggable();
-    $( ".puzzle-piece" ).droppable({
-      //tolerance: 'fit';
-      /*drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" );
-      }
-
-    })
-
-
-
-
-
-
-;*/
 
 
